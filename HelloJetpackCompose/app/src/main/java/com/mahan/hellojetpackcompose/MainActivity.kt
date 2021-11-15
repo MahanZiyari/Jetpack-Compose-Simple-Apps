cf33b2c1a6654ad5ea8ee15fc95f96a3092db894
Mahan Ziyari <maziyar.mz.021@gmail.com>
2021-11-15 10:03:41 +0330

    Hello Jetpack Compose

---
+++ HelloJetpackCompose/app/src/main/java/com/mahan/hellojetpackcompose/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.mahan.hellojetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.mahan.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Greeting(name = "Jetpack Compose")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.background) {
+        // Greeting("Android")
+        Text(
+            text = "Hello $name!",
+            style = TextStyle(
+                color = MaterialTheme.colors.onSurface,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Black,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.Center
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HelloJetpackComposeTheme {
+        Greeting("Mahan")
+    }
+}
